7ecd8e000379b7dddb610a1f60b7fb8996d0f71f
gnastacast <gnastacast@gmail.com>
2016-03-09 02:03:44 -0500

    Removed old files and added .gitignore

--- qt/build/rqt_example_cpp/catkin_generated/package.cmake
+++
@@ -1,7 +0,0 @@
-set(_CATKIN_CURRENT_PACKAGE "rqt_example_cpp")
-set(rqt_example_cpp_MAINTAINER "Lucas Walter <lucasw_backwards@gmail.com>")
-set(rqt_example_cpp_DEPRECATED "")
-set(rqt_example_cpp_VERSION "0.0.0")
-set(rqt_example_cpp_BUILD_DEPENDS "roscpp" "rqt_gui" "rqt_gui_cpp" "std_msgs")
-set(rqt_example_cpp_RUN_DEPENDS "roscpp" "rqt_gui" "rqt_gui_cpp" "std_msgs")
-set(rqt_example_cpp_BUILDTOOL_DEPENDS "catkin")
