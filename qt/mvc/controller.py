6a16f54a25f73a26478a54ff112cc0054db77fd5
gnastacast <gnastacast@gmail.com>
2016-03-09 01:57:35 -0500

    Made code work with newer versions of VTK and windows

--- qt/mvc/controller.py
+++ qt/mvc/controller.py
@@ -1,32 +1,41 @@
+import time
+from sys import platform
+from subprocess import call
 import numpy as np
 import cv2
 from PyQt4 import QtCore, QtGui
 from vtkTools import numpyToVtkImage
-from subprocess import call
-import time
 
 class MainController(object):
 
     def __init__(self, model):
         self.model = model
+        # Video update thread
         self.videoUpdater = self._VideoUpdateThread(model)
         self.videoUpdater.start()
+        # Variables for drawing mask
         self.drawingBox = False
         self.drawingLine = False
         self.brushColor = int(cv2.GC_FGD)
-        self.needsUpdate = False;
 
     # called from view class
     def changeMasking(self, checked):
-        # put control logic here
+        # Turn masking variable in model off and on
         self.model.masking = checked
         self.model.announce_update()
+        # Hide actor while masking
         if checked:
             self.model.stlActor.VisibilityOff()
         else:
             self.model.stlActor.VisibilityOn()
 
     def changeAutoCamera(self,checked):
+        ''' Hacky function that turns auto exposure and auto white white balance
+            off and on. This is camera and operating system specific, so it will
+            probably need to be changed in order to work with your setup.
+        '''
+        if platform != 'linux':
+            return
         print checked
         if checked:
             # Turn on all automatic features
@@ -38,13 +47,13 @@ class MainController(object):
             call(["v4l2-ctl", "--set-ctrl", "exposure_auto=1"])
             call(["v4l2-ctl", "--set-ctrl", "focus_auto=0"])
             call(["v4l2-ctl", "--set-ctrl", "white_balance_temperature_auto=0"])
+    
     def drawOnMask(self, pos):
         cv2.circle(self.model.mask,pos, 5, (self.brushColor), -1)
-
+    
     def mouseEvent(self,eventType,interactor):
-
-        # if not in masking mode, do nothing
-        if not(self.model.masking):
+        # if not in masking mode or not updating, do nothing
+        if not(self.model.masking or self.videoUpdater.running):
             return
         # Get position of event
         mousePos = interactor.GetEventPosition()
@@ -52,48 +61,63 @@ class MainController(object):
 
         if eventType == 'mouseMove':
             if self.drawingBox == True:
+                # Set rectangular masking area
                 self.model.rect = self.model.rect[0:2]+mousePos
             if self.drawingLine == True:
                 self.drawOnMask(mousePos)
 
         elif eventType == 'leftButtonPress':
             if interactor.GetShiftKey():
+                # Mark as foreground
                 self.drawingLine = True
                 self.brushColor = int(cv2.GC_FGD)
                 self.drawOnMask(mousePos)
             elif interactor.GetControlKey():
+                # Mark as background
                 self.drawingLine = True
                 self.brushColor = int(cv2.GC_BGD)
                 self.drawOnMask(mousePos)
             else:
+                # Begin marking rectangular masking area
+                self.model.mask = np.ones(self.model.imgDims[::-1],np.uint8)*cv2.GC_PR_FGD
                 self.drawingBox = True
                 self.model.rect = mousePos + mousePos
 
         elif eventType == 'leftButtonRelease':
-            if self.drawingBox == True:
-                self.model.mask = np.ones(self.model.imgDims[::-1],np.uint8)*cv2.GC_PR_FGD
-            self.drawingBox = False
-            self.drawingLine = False
-            self.needsUpdate = True
+            # Check rectangular mask area is not zero
+            rect = self.model.rect
+            area = (rect[2] - rect[0]) * (rect[3] - rect[1])
+            if area!=0:
+                # Update masked frame and clear drawing variables
+                self.model.maskedFrame = self._removeBG(self.model.videoFrame)
+                self.drawingBox = False
+                self.drawingLine = False
 
     def _removeBG(self, frame):
-        startCorner = self.maskingTool.boxStart
-        endCorner = self.maskingTool.boxEnd
+        # Stop update to stop commands from piling up
+        self.videoUpdater.running = False
+        # Begin with a new mask that is all background
         img = frame.copy()
         mask = np.ones(img.shape[:2],np.uint8)*cv2.GC_BGD
+        # Replace everything inside the box selection with data from the current mask
+        startCorner = self.model.rect[0:2]
+        endCorner = self.model.rect[2:4]
         mask[startCorner[1]:endCorner[1], startCorner[0]:endCorner[0]] = \
-            self.maskingTool.mask[startCorner[1]:endCorner[1],startCorner[0]:endCorner[0]]
-        rect = startCorner+endCorner
+            self.model.mask[startCorner[1]:endCorner[1],startCorner[0]:endCorner[0]]
+        # Perform grabcut algorithm to separate background and foreground
         tmp1 = np.zeros((1, 13 * 5))
         tmp2 = np.zeros((1, 13 * 5))
-        print "Started Grabcut"
-        startCorner = self.maskingTool.masking=False
-        cv2.grabCut(img,mask,rect,tmp1,tmp2,5,mode=cv2.GC_INIT_WITH_MASK)
-        startCorner = self.maskingTool.masking=True
-        print "Finished Grabcut"
+        cv2.grabCut(img,mask,self.model.rect,tmp1,tmp2,5,mode=cv2.GC_INIT_WITH_MASK)
+        # Redden areas that are masked out
         mask2 = np.where((mask==2)|(mask==0),0.25,1)
         img[:,:,0:2] = img[:,:,0:2]*mask2[:,:,np.newaxis]
+        # Restart update
+        self.videoUpdater.running = True
+        self.videoUpdater.start()
         return img.astype('uint8')
+
+    def register(self):
+        return
 
     class _VideoUpdateThread(QtCore.QThread):
 
@@ -114,13 +138,14 @@ class MainController(object):
         def updateVideo(self):
             ret, frame = self.model.cap.read()
             self.model.videoFrame = frame.copy()
+            self.model.maskedFrame = frame.copy()
             numpyToVtkImage(frame,self.model.bgImage)
             self.VTK_updated.emit(1)
 
         def updateMasking(self):
             boxStart = self.model.rect[0:2]
             boxEnd = self.model.rect[2:4]
-            tempFrame = self.model.videoFrame.copy()
+            tempFrame = self.model.maskedFrame.copy()
             cv2.rectangle(tempFrame,boxStart,boxEnd,(0,255,0),3)
             tempFrame[self.model.mask==cv2.GC_FGD] = (0,255,0)
             tempFrame[self.model.mask==cv2.GC_BGD] = (0,0,255)
--- qt/mvc/vtkTools.py
+++ qt/mvc/vtkTools.py
@@ -5,29 +5,52 @@ import cv2
 
 # Sets up a three channel empty vtkImageData object
 def makeVtkImage(imgDims):
+    ''' Creates a blank vtkImageData object of specified dimensions
+        Input: imgDims (int, int): desired width and height
+        Output: imData (vtkImageData): empty VTK image with three
+                channels for RGB
+    '''
     imData = vtk.vtkImageData()
-    imData.SetDimensions(imgDims[0], imgDims[1], 1) #set dimensions as necessary
+    imData.SetDimensions(imgDims[0], imgDims[1],1) #set dimensions as necessary
     imData.SetOrigin(0,0,0) #set origin as necessary
     imData.SetSpacing(1, 1, 1) #set spacing as necessary
-    imData.SetNumberOfScalarComponents( 3 )
-    imData.SetScalarTypeToUnsignedChar()
-    imData.Update()
+    # Set number of channels (3)
+    if vtk.VTK_MAJOR_VERSION <= 5:
+        imData.SetNumberOfScalarComponents(3)
+        imData.SetScalarTypeToUnsignedChar()
+        imData.Update()
+    else:
+        imData.AllocateScalars(vtk.VTK_UNSIGNED_CHAR, 3)
     return imData
 
 def numpyToVtkImage(numpyData,VTKImageData):
+    ''' Copies a numpy array to a vtkImageData object in place.
+        Used for updating existing vtkImageData with new pixel
+        data.
+        https://pyscience.wordpress.com/2014/09/06/numpy-to-vtk-converting-your-numpy-arrays-to-vtk-arrays-and-files/
+        was extremely useful
+
+    '''
+    # Data from CV comes as BGR and vtkImageData displays as RGB so we convert
     rgb = cv2.cvtColor(numpyData, cv2.COLOR_BGR2RGB)
-    VTK_data = numpy_support.numpy_to_vtk(num_array=rgb[::-1].ravel(),deep=True, array_type=vtk.VTK_CHAR)
+    # Use numpy_support module to get a vtkDataArray object containing pixel data
+    VTK_data = numpy_support.numpy_to_vtk(num_array=rgb[::-1].ravel(),deep=True, array_type=vtk.VTK_UNSIGNED_CHAR)
+    # Set number of channels in pixel data
+    VTK_data.SetNumberOfComponents(3)
     VTKImageData.GetPointData().SetScalars(VTK_data)
-    return VTKImageData
 
 def vtkImageToNumpy(VTKImageData,numpyData):
+    ''' Copies pixel data from a vtkImageData into a
+        numpy array in place
+    '''
     size = VTKImageData.GetDimensions()
     numpyData = numpy_support.vtk_to_numpy(VTK_data)
     numpyData = numpyData.reshape((size[1],size[0],3))
-    return numpyData
 
 def actorFromStl(stlPath):
-    # Read in STL file
+    ''' Reads in an STL file and returns a vtkActor
+        object that the vtkRenderer can use
+    '''
     reader = vtk.vtkSTLReader()
     reader.SetFileName(stlPath)
     mapper = vtk.vtkPolyDataMapper()
@@ -41,7 +64,20 @@ def actorFromStl(stlPath):
     return actor
 
 def setupRenWinForRegistration(renWin,bgImage,actor,camMatrix):
+    ''' Sets up the render window for registration in place
+        Input:
+            renWin (vtkRenderWindow): render window to set up with
+                                      actors and renderers
+            bgImage (vtkImageData): image to put in the background
+                                    that will be updated with video
+                                    footage and masking feedback
+            actor (vtkActor): Actor that functions as mapper for the
+                              STL file we are registering.
+            camMatrix (np.ndarray): 4x4 camera intrinsic matrix from
+                                    OpenCV calibration
+    '''
     renWin.SetNumberOfLayers(2)
+
     # Create foreground renderer for stl object
     ren=vtk.vtkRenderer()
     ren.SetLayer(1)
@@ -51,7 +87,6 @@ def setupRenWinForRegistration(renWin,bgImage,actor,camMatrix):
     renWin.SetSize(imgDims[0],imgDims[1])
     camera = _cameraFromMatrix(camMatrix,imgDims,imgDims)
     ren.SetActiveCamera(camera)
-    #ren.SetActiveCamera(camera)
 
     # Create background renderer for static object
     backgroundRen = vtk.vtkRenderer()
@@ -78,10 +113,11 @@ def _makeImagePlane(imageData) :
         was the basis for this function
     '''
     imageActor = vtk.vtkImageActor()
+    
     if vtk.VTK_MAJOR_VERSION <= 5 :
-      imageActor.SetInput(imageData);
+        imageActor.SetInput(imageData)
     else :
-      imageActor.SetInputData(imageData);
+        imageActor.SetInputData(imageData)
 
     origin = imageData.GetOrigin()
     spacing = imageData.GetSpacing()
@@ -92,11 +128,11 @@ def _makeImagePlane(imageData) :
 
     xc = origin[0] + 0.5*(extent[0] + extent[1])*spacing[0]
     yc = origin[1] + 0.5*(extent[2] + extent[3])*spacing[1]
-    yd = (extent[3] - extent[2] + 1)*spacing[1];
+    yd = (extent[3] - extent[2] + 1)*spacing[1]
     d = camera.GetDistance()
     camera.SetParallelScale(0.5*yd)
     camera.SetFocalPoint(xc,yc,0.0)
-    camera.SetPosition(xc,yc,d);
+    camera.SetPosition(xc,yc,d)
 
     return camera, imageActor
 
@@ -127,39 +163,35 @@ def _cameraFromMatrix(camMatrix, imageDims, windowDims) :
     camera.SetViewAngle(viewAngle)
 
     #Set window center
-    py = 0;
-    height = 0;
+    py = 0
+    height = 0
 
     if imageDims[0] != windowDims[0] or imageDims[1] != windowDims[1] :
-
         factor = float(windowDims[1])/float(imageDims[1])
         px = factor * camMatrix[0,2]
         width = windowDims[0]
-        expectedWindowSize = np.round(factor * float(imageDims[0]));
+        expectedWindowSize = np.round(factor * float(imageDims[0]))
         if( expectedWindowSize != windowDims[0] ):
-            diffX = (windowDims[0] - expectedWindowSize) / 2;
+            diffX = (windowDims[0] - expectedWindowSize) / 2
             px = px + diffX
-
         py = factor * camMatrix[1,2]
         height = windowDims[1]
 
     else :
         px = camMatrix[0,2]
         width = imageDims[0]
-
         py = camMatrix[1,2]
         height = imageDims[1]
 
-    cx = width - px;
+    cx = width - px
     cy = py
 
     windowCenter = [0,0]
-
     windowCenter[0] = cx / ( ( width-1)/2 ) - 1 
     windowCenter[1] = cy / ( ( height-1)/2 ) - 1
-
     camera.SetWindowCenter(windowCenter[0],windowCenter[1])
 
+    # Set camera to look forward from center
     camera.SetPosition(0,0,0)
     camera.SetFocalPoint(0,0,1)
 
