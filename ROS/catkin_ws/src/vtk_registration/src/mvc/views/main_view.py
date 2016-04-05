fbee40d4e93d9b1855cb56d2a3770894ac7fe0b6
Feroze Naina <ferozenaina@gmail.com>
2016-04-05 15:45:13 -0400

    Moved QT code to ROS directory

---
+++ ROS/catkin_ws/src/vtk_registration/src/mvc/views/main_view.py
@@ -0,0 +1,129 @@
+from PyQt4 import QtGui, QtCore
+from vtk import vtkInteractorStyleTrackballActor
+from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+from ui_main_view import Ui_MainView
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class MainView(QtGui.QMainWindow):
+
+    # properties to read/write widget values
+    @property
+    def masking(self):
+        return self.ui.actionMasking.isChecked()
+    @masking.setter
+    def masking(self, value):
+        self.ui.actionMasking.setChecked(value)
+    @property
+    def autoCamera(self):
+        return self.ui.actionAutoCamera.isChecked()
+    @autoCamera.setter
+    def autoCamera(self, value):
+        self.ui.actionAutoCamera.setChecked(value)
+
+
+    def __init__(self, model, mainCtrl):
+        self.model = model
+        self.mainCtrl = mainCtrl
+        super(MainView, self).__init__()
+        self.buildUI()
+        # Set up controller update thread
+        self.mainCtrl.bgUpdater.VTK_updated.connect(self.renderVTK)
+        # register func with model for future model update announcements
+        self.model.subscribe_update_func(self.updateUiFromModel)
+    def buildUI(self):
+        self.ui = Ui_MainView()
+        self.ui.setupUi(self)
+        # connect signals to methods
+        self.ui.actionMasking.changed.connect(self.onMasking)
+        self.ui.actionAutoCamera.changed.connect(self.onSetCameraParams)
+        self.ui.actionFineRegistration.triggered.connect(self.onFineRegistration)
+        # Replace vertical layout placeholder with a QVTKRenderWindowInteractor
+        self.ui.qvtkWidget = QVTKRenderWindowInteractor(self.ui.centralwidget)
+        self.ui.qvtkWidget.setObjectName(_fromUtf8("qvtkWidget"))
+        imgDims = self.model.bgImage.GetDimensions()
+        self.ui.qvtkWidget.setMinimumWidth(imgDims[0])
+        self.ui.qvtkWidget.setMaximumWidth(imgDims[0])
+        self.ui.qvtkWidget.setMinimumHeight(imgDims[1])
+        self.ui.qvtkWidget.setMaximumHeight(imgDims[1])
+        self.ui.horizontalLayout.addWidget(self.ui.qvtkWidget)
+        # Add this render window to the model
+        self.model.setRenWin(self.ui.qvtkWidget.GetRenderWindow())
+        self.model.renWin = self.ui.qvtkWidget.GetRenderWindow()
+        # Replace default interactor style to send messages to controller
+        self.iren = self.ui.qvtkWidget.GetRenderWindow().GetInteractor()
+        self.iren.SetInteractorStyle(MainViewInteractorStyle(self.mainCtrl))
+
+    def onMasking(self):
+        self.mainCtrl.changeMasking(self.masking)
+
+    def onSetCameraParams(self):
+        self.mainCtrl.changeAutoCamera(self.autoCamera)
+
+    def onFineRegistration(self):
+        self.mainCtrl.register()
+
+    def updateUiFromModel(self):
+        self.masking = self.model.masking
+
+    def renderVTK(self):
+        self.ui.qvtkWidget.GetRenderWindow().Render()
+
+    def closeEvent(self,event):
+        self.mainCtrl.bgUpdater.running = False
+        self.mainCtrl.bgUpdater.wait()
+        self.mainCtrl.bgUpdater.terminate()
+        self.mainCtrl.bgUpdater.wait()
+        self.iren.TerminateApp()
+        QtGui.qApp.processEvents()
+        super(MainView, self).closeEvent(event)
+
+    def show(self):
+        ''' This adds to the default show function to initialize the interactor
+            after MainView is shown to avoid a segfault caused if qvtkWidget's
+            interactor is initialied before the window is shown.
+        '''
+        super(MainView, self).show()
+        self.iren.Initialize()
+
+
+class MainViewInteractorStyle(vtkInteractorStyleTrackballActor):
+ # Class defining a modified interactor style for registration
+    def __init__(self, controller):
+        #self.AddObserver("KeyPressEvent", self.keyPressEvent)
+        self.AddObserver("RightButtonPressEvent", self.rightButtonPressEvent)
+        self.AddObserver("RightButtonReleaseEvent", self.rightButtonReleaseEvent)
+        self.AddObserver("LeftButtonPressEvent", self.leftButtonPressEvent)
+        self.AddObserver("LeftButtonReleaseEvent", self.leftButtonReleaseEvent)
+        self.AddObserver("MouseMoveEvent", self.mouseMoveEvent)
+        self.controller = controller
+    # This removes camera dolly actions and replaces them with object dolly actions
+    # This way the camera stays still no matter what you click.
+    def rightButtonPressEvent(self,obj,event):
+        self.OnMiddleButtonDown()
+        return
+    def rightButtonReleaseEvent(self,obj,event):
+        self.OnMiddleButtonUp()
+        return
+    # This sends press events to the controller
+    def leftButtonPressEvent(self,obj,event):
+        # if not in masking mode or not updating, do nothing
+        if self.controller.model.masking and self.controller.bgUpdater.running:
+            self.controller.mouseEvent('leftButtonPress',self.GetInteractor())
+        elif self.controller.bgUpdater.running:
+            self.OnLeftButtonDown()
+        return
+    def leftButtonReleaseEvent(self,obj,event):
+        if self.controller.model.masking and self.controller.bgUpdater.running:
+            self.controller.mouseEvent('leftButtonRelease',self.GetInteractor())
+        elif self.controller.bgUpdater.running:
+            self.OnLeftButtonUp()
+        return
+    def mouseMoveEvent(self,obj,event):
+        if self.controller.model.masking and self.controller.bgUpdater.running:
+            self.controller.mouseEvent('mouseMove',self.GetInteractor())
+        elif self.controller.bgUpdater.running:
+            self.OnMouseMove()
+        return
---
+++ ROS/catkin_ws/src/vtk_registration/src/mvc/vtkTools.py
@@ -0,0 +1,223 @@
+import numpy as np
+from vtk.util import numpy_support
+import vtk
+import cv2
+
+# Sets up a three channel empty vtkImageData object
+def makeVtkImage(imgDims):
+    ''' Creates a blank vtkImageData object of specified dimensions
+        Input: imgDims (int, int): desired width and height
+        Output: imData (vtkImageData): empty VTK image with three
+                                       channels for RGB
+    '''
+    imData = vtk.vtkImageData()
+    imData.SetDimensions(imgDims[0], imgDims[1],1) #set dimensions as necessary
+    imData.SetOrigin(0,0,0) #set origin as necessary
+    imData.SetSpacing(1, 1, 1) #set spacing as necessary
+    # Set number of channels (3)
+    if vtk.VTK_MAJOR_VERSION <= 5:
+        imData.SetNumberOfScalarComponents(3)
+        imData.SetScalarTypeToUnsignedChar()
+        imData.Update()
+    else:
+        imData.AllocateScalars(vtk.VTK_UNSIGNED_CHAR, 3)
+    return imData
+
+def numpyToVtkImage(numpyData,VTKImageData):
+    ''' Copies a numpy array to a vtkImageData object in place.
+        Used for updating existing vtkImageData with new pixel
+        data.
+        https://pyscience.wordpress.com/2014/09/06/numpy-to-vtk-converting-your-numpy-arrays-to-vtk-arrays-and-files/
+        was extremely useful
+    '''
+    if len(numpyData.shape)==3 and numpyData.shape[2] == 3:
+        # Data from CV comes as BGR and vtkImageData displays as RGB so we convert
+        rgb = cv2.cvtColor(numpyData, cv2.COLOR_BGR2RGB)
+        # Get a vtkDataArray object containing numpy pixel data
+        VTK_data = numpy_support.numpy_to_vtk(num_array=rgb[::-1].ravel(),
+                                              deep=True, 
+                                              array_type=vtk.VTK_UNSIGNED_CHAR)
+        # Set number of channels in pixel data
+        VTK_data.SetNumberOfComponents(numpyData.shape[2])
+    else:
+        #Get a vtkDataArray object containing pixel data
+        VTK_data = numpy_support.numpy_to_vtk(num_array=numpyData[::-1].ravel(),
+                                              deep=True, 
+                                              array_type=vtk.VTK_UNSIGNED_CHAR)
+    VTKImageData.GetPointData().SetScalars(VTK_data)
+
+def vtkImageToNumpy(VTKImageData):
+    ''' Copies pixel data from a vtkImageData and returns a new numpy array
+    '''
+    size = VTKImageData.GetDimensions()
+    shape = (size[1],size[0],VTKImageData.GetNumberOfScalarComponents())
+    VTK_data = VTKImageData.GetPointData().GetScalars()
+    numpyData = numpy_support.vtk_to_numpy(VTK_data)
+    numpyData = numpyData.reshape(shape)
+    numpyData = numpyData[::-1]
+    return numpyData
+
+def actorFromStl(stlPath):
+    ''' Reads in an STL file and returns a vtkActor
+        object that the vtkRenderer can use
+    '''
+    reader = vtk.vtkSTLReader()
+    reader.SetFileName(stlPath)
+    mapper = vtk.vtkPolyDataMapper()
+    if vtk.VTK_MAJOR_VERSION <= 5:
+        mapper.SetInput(reader.GetOutput())
+    else:
+        mapper.SetInputConnection(reader.GetOutputPort())
+    # Create actor that we will position manually
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    return actor
+
+def setupRenWinForRegistration(renWin,bgImage,actor,camMatrix):
+    ''' Sets up the render window for registration in place
+        Input:
+            renWin (vtkRenderWindow): render window to set up with
+                                      actors and renderers
+            bgImage (vtkImageData): image to put in the background
+                                    that will be updated with video
+                                    footage and masking feedback
+            actor (vtkActor): Actor that functions as mapper for the
+                              STL file we are registering.
+            camMatrix (np.ndarray): 4x4 camera intrinsic matrix from
+                                    OpenCV calibration
+    '''
+    renWin.SetNumberOfLayers(2)
+
+    # Create foreground renderer for stl object
+    ren=vtk.vtkRenderer()
+    ren.SetLayer(1)
+    renWin.AddRenderer(ren)
+    ren.AddActor(actor)
+    imgDims = bgImage.GetDimensions()
+    renWin.SetSize(imgDims[0],imgDims[1])
+    camera = _cameraFromMatrix(camMatrix,imgDims,imgDims)
+    ren.SetActiveCamera(camera)
+
+    # Create background renderer for static object
+    backgroundRen = vtk.vtkRenderer()
+    backgroundRen.SetLayer(0)
+    backgroundRen.InteractiveOff()
+    backgroundRen.SetBackground(1, 1, 1)
+    # Create background object for showing video feed.
+    bgCamera, backgroundActor = _makeImagePlane(bgImage)
+    # Setup camera
+    backgroundRen.AddActor(backgroundActor)
+    backgroundRen.SetActiveCamera(bgCamera)
+    # Add background renderer to window
+    renWin.AddRenderer(backgroundRen)
+    
+    # Way of getting camera: renWin.GetRenderers().GetFirstRenderer()
+    return renWin
+
+class zBuff:
+    def __init__(self,renWin):
+        self.renWin = renWin
+        self.zBuffFilter = vtk.vtkWindowToImageFilter()
+        self.zBuffFilter.SetInput(self.renWin)
+        self.zBuffFilter.SetInputBufferTypeToZBuffer()
+
+        self.scaledZBuff = vtk.vtkImageShiftScale()
+        self.scaledZBuff.SetOutputScalarTypeToUnsignedChar()
+        self.scaledZBuff.SetInputConnection(self.zBuffFilter.GetOutputPort())
+        self.scaledZBuff.SetShift(0)
+        self.scaledZBuff.SetScale(255)
+
+    def GetOutput(self):
+        self.renWin.Render()
+        self.zBuffFilter.Modified()
+        self.scaledZBuff.Update()
+        return self.scaledZBuff.GetOutput()
+
+# Loads an image and returns a plane object and a camera pointed towards it
+def _makeImagePlane(imageData) :
+    ''' Takes a vtkImageData object and
+        returns a plane textured with that image
+        and a camera for the background renderer
+        http://www.vtk.org/Wiki/VTK/Examples/Cxx/Images/BackgroundImage
+        was the basis for this function
+    '''
+    imageActor = vtk.vtkImageActor()
+    
+    if vtk.VTK_MAJOR_VERSION <= 5 :
+        imageActor.SetInput(imageData)
+    else :
+        imageActor.SetInputData(imageData)
+
+    origin = imageData.GetOrigin()
+    spacing = imageData.GetSpacing()
+    extent = imageData.GetExtent()
+
+    camera = vtk.vtkCamera()
+    camera.ParallelProjectionOn()
+
+    xc = origin[0] + 0.5*(extent[0] + extent[1])*spacing[0]
+    yc = origin[1] + 0.5*(extent[2] + extent[3])*spacing[1]
+    yd = (extent[3] - extent[2] + 1)*spacing[1]
+    d = camera.GetDistance()
+    camera.SetParallelScale(0.5*yd)
+    camera.SetFocalPoint(xc,yc,0.0)
+    camera.SetPosition(xc,yc,d)
+
+    return camera, imageActor
+
+# Sets up a camera based on a camera intrinsics matrix from OpenCV
+def _cameraFromMatrix(camMatrix, imageDims, windowDims) :
+    ''' Takes a camera matrix derived from OpenCV
+        returns a camera for registration
+        http://stackoverflow.com/questions/25539898/how-to-apply-the-camera-pose-transformation-computed-using-epnp-to-the-vtk-camer
+        was a great help
+
+        Input: 
+            camMatrix (np.ndarray) = 4x4 camera intrinsic matrix
+            imageDims ((float, float))  = width and height of image to register
+            imageDims ((float, float))  = width and height of render window
+
+        Output:
+            camera (vtk.vtkCamera) = camera to use for registration
+    '''
+    # Initialize camera
+    camera = vtk.vtkCamera()
+
+    # Set view angle
+    focalLengthY = camMatrix[1,1]
+    if( windowDims[1] != imageDims[1] ):
+        factor = float(windowDims[1])/float(imageDims[1])
+        focalLengthY = camMatrix[1,1] * factor
+    viewAngle = np.arctan((windowDims[1]/2.0)/focalLengthY)*360/np.pi
+    camera.SetViewAngle(viewAngle)
+
+    #Set window center
+    py = 0
+    height = 0
+    if imageDims[0] != windowDims[0] or imageDims[1] != windowDims[1] :
+        factor = float(windowDims[1])/float(imageDims[1])
+        px = factor * camMatrix[0,2]
+        width = windowDims[0]
+        expectedWindowSize = np.round(factor * float(imageDims[0]))
+        if( expectedWindowSize != windowDims[0] ):
+            diffX = (windowDims[0] - expectedWindowSize) / 2
+            px = px + diffX
+        py = factor * camMatrix[1,2]
+        height = windowDims[1]
+    else :
+        px = camMatrix[0,2]
+        width = imageDims[0]
+        py = camMatrix[1,2]
+        height = imageDims[1]
+    cx = width - px
+    cy = py
+    windowCenter = [0,0]
+    windowCenter[0] = cx / ( ( width-1)/2 ) - 1 
+    windowCenter[1] = cy / ( ( height-1)/2 ) - 1
+    camera.SetWindowCenter(windowCenter[0],windowCenter[1])
+
+    # Set camera to look forward from center
+    camera.SetPosition(0,0,0)
+    camera.SetFocalPoint(0,0,1)
+
+    return camera
